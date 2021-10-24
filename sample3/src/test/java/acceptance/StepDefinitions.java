107729d4f36aa81e8e77639c2192c1d01d18a82a
imharsh2005 <62921022+imharsh2005@users.noreply.github.com>
2021-10-24 13:32:32 -0400

    Update StepDefinitions.java

--- sample3/src/test/java/acceptance/StepDefinitions.java
+++ sample3/src/test/java/acceptance/StepDefinitions.java
@@ -23,7 +23,7 @@ public class StepDefinitions {
         this.b = b;
     }
 
-    @When("^the calculator sum them$")
+    @When("^the calculator sums them$")
     public void the_calculator_sums_them() throws Throwable {
         String url = String.format("%s/sum?a=%s&b=%s", server, a, b);
         result = restTemplate.getForObject(url, String.class);
